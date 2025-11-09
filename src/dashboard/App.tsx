91266276650989797e2f28e603281e966c1b4a2a
uoftWANGTIANMING <tianming.wang@mail.utoronto.ca>
2025-11-09 13:59:56 -0500

    Update dashboard refresh interval to 10 seconds

--- src/dashboard/App.tsx
+++ src/dashboard/App.tsx
@@ -90,7 +90,7 @@ function App() {
 
   useEffect(() => {
     fetchMetrics();
-    const interval = setInterval(() => fetchMetrics(false), 5000);
+    const interval = setInterval(() => fetchMetrics(false), 10000);
     return () => clearInterval(interval);
   }, [fetchMetrics]);
 
